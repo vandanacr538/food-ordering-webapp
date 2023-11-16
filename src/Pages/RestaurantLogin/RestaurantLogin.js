aac055ee20a8c685dbcdb920c3ba9f8a96de1376
vandanacr538 <vandanacr538@gmail.com>
2023-11-16 18:56:51 +0530

    Integrated Restaurant SignUp & Login APIs to Frontend

--- src/Pages/RestaurantLogin/RestaurantLogin.js
+++ src/Pages/RestaurantLogin/RestaurantLogin.js
@@ -1,38 +1,176 @@
-import React from 'react'
+import React, { useState } from "react";
 import "../../CommonStyle/loginandsignup.css";
 import restaurant from "../../Assets/restaurant.webp";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export default function RestaurantLogin() {
   const navigate = useNavigate();
-  const gotoRestaurantSignUpPage=()=>{
+  const [restaurantLoginData, setRestaurantLoginData] = useState({
+    restaurant_email: "",
+    restaurant_password: "",
+  });
+  const [restLoginFieldsError, setRestLoginFieldsError] = useState({});
+  const [restLoginAuthError, setRestLoginAuthError] = useState("");
+  const restLoginValidDataObj = {
+    isRestEmailValid: false,
+    isRestPasswordValid: false,
+  };
+
+  const handleChangeRestLoginData = (e) => {
+    setRestaurantLoginData((previousValue) => ({
+      ...previousValue,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const validateRestEmail = () => {
+    const emailRegx =
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (restaurantLoginData.restaurant_email === "") {
+      setRestLoginFieldsError((previousError) => ({
+        ...previousError,
+        restaurant_emailError: "Enter restaurant email",
+      }));
+      restLoginValidDataObj["isRestEmailValid"] = false;
+    } else if (emailRegx.test(restaurantLoginData.restaurant_email) === false) {
+      setRestLoginFieldsError((previousError) => ({
+        ...previousError,
+        restaurant_emailError: "Invalid email address!, please check",
+      }));
+      restLoginValidDataObj["isRestEmailValid"] = false;
+    } else {
+      setRestLoginFieldsError((previousError) => ({
+        ...previousError,
+        restaurant_emailError: "",
+      }));
+      restLoginValidDataObj["isRestEmailValid"] = true;
+    }
+  };
+  const validateRestPassword = () => {
+    if (restaurantLoginData.restaurant_password === "") {
+      setRestLoginFieldsError((previousError) => ({
+        ...previousError,
+        restaurant_passwordError: "Enter restaurant password",
+      }));
+      restLoginValidDataObj["isRestPasswordValid"] = false;
+    } else {
+      setRestLoginFieldsError((previousError) => ({
+        ...previousError,
+        restaurant_passwordError: "",
+      }));
+      restLoginValidDataObj["isRestPasswordValid"] = true;
+    }
+  };
+  const handleClickRestLogin = async () => {
+    setRestLoginAuthError("");
+    validateRestEmail();
+    validateRestPassword();
+    console.log(restLoginValidDataObj);
+    if (Object.values(restLoginValidDataObj).every(Boolean)) {
+      try {
+        const result = await axios.post(
+          "http://localhost:8080/restaurant/login",
+          restaurantLoginData
+        );
+        if (result.status === 200) {
+          console.log(result.data);
+          console.log("navigate to restaurant home page");
+        }
+      } catch (e) {
+        if (e.response.status === 401) {
+          setRestLoginAuthError(e.response.data.msg);
+        } else if (e.response.status === 403) {
+          setRestLoginAuthError(e.response.data.msg);
+        } else {
+          setRestLoginAuthError(e.response.data.msg);
+        }
+      }
+    }
+  };
+  const gotoRestaurantSignUpPage = () => {
     navigate("/restaurant/signup");
-  }
+  };
 
   return (
-    <div className='login-signup-main-container'>
-        <div className='login-signup-image-box'>
-            <img src={restaurant} alt="restaurantimage" className='login-signup-page-img'></img>
+    <div className="login-signup-main-container">
+      <div className="login-signup-image-box">
+        <img
+          src={restaurant}
+          alt="restaurantimage"
+          className="login-signup-page-img"
+        ></img>
+      </div>
+      <div className="login-signup-container">
+        <div className="login-signup-form-box restaurant-login-signup-form-box">
+          <h1>Restaurant Login</h1>
+          <div className="login-signup-form-fields">
+            <label htmlFor="restaurant-email">Restaurant Email</label>
+            <input
+              type="email"
+              id="restaurant-email"
+              className="login-signup-form-input"
+              name="restaurant_email"
+              onChange={handleChangeRestLoginData}
+            ></input>
+            <p
+              className={
+                restLoginFieldsError.restaurant_emailError === ""
+                  ? "hide-data"
+                  : "display-input-data-error"
+              }
+            >
+              {restLoginFieldsError.restaurant_emailError}
+            </p>
+          </div>
+          <div className="login-signup-form-fields">
+            <label htmlFor="restaurant-password">Password</label>
+            <input
+              type="password"
+              id="restaurant-password"
+              className="login-signup-form-input"
+              name="restaurant_password"
+              onChange={handleChangeRestLoginData}
+            ></input>
+            <p
+              className={
+                restLoginFieldsError.restaurant_passwordError === ""
+                  ? "hide-data"
+                  : "display-input-data-error"
+              }
+            >
+              {restLoginFieldsError.restaurant_passwordError}
+            </p>
+            <p
+              className={
+                restLoginAuthError === ""
+                  ? "hide-data"
+                  : "display-login-signup-auth-error"
+              }
+            >
+              {restLoginAuthError}
+            </p>
+          </div>
+          <div className="login-signup-form-complete-btn-box">
+            <button
+              className="login-signup-form-complete-btn restaurant-login-signup-btn"
+              onClick={handleClickRestLogin}
+            >
+              Sign in
+            </button>
+          </div>
+          <div className="login-signup-link-box">
+            <p>
+              Don't have an account?
+              <span
+                className="login-sign-up-link"
+                onClick={gotoRestaurantSignUpPage}
+              >
+                Sign Up
+              </span>
+            </p>
+          </div>
         </div>
-        <div className='login-signup-container'>
-            <div className='login-signup-form-box restaurant-login-signup-form-box'>
-                <h1>Restaurant Login</h1>
-                <div className='login-signup-form-fields'>
-                    <label htmlFor="restaurant-email">Restaurant Email</label>
-                    <input type='email' id='restaurant-email' className='login-signup-form-input' name="restaurant-email" ></input>
-                </div>
-                <div className='login-signup-form-fields'>
-                    <label htmlFor="restaurant-password">Password</label>
-                    <input type='password' id='restaurant-password' className='login-signup-form-input' name="restaurant-password" ></input>
-                </div>
-                <div className='login-signup-form-complete-btn-box'>
-                    <button className='login-signup-form-complete-btn restaurant-login-signup-btn'>Sign in</button>
-                </div>
-                <div className='login-signup-link-box'>
-                    <p>Don't have an account?<span className='login-sign-up-link' onClick={gotoRestaurantSignUpPage}>Sign Up</span></p>
-                </div>
-            </div>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
--- src/Pages/RestaurantSignUp/RestaurantSignUp.js
+++ src/Pages/RestaurantSignUp/RestaurantSignUp.js
@@ -1,53 +1,356 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 import "../../CommonStyle/loginandsignup.css";
 import restaurant from "../../Assets/restaurant.webp";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 export default function RestaurantSignUp() {
   const navigate = useNavigate();
-  const gotoRestaurantLoginPage=()=>{
+  const [restSignUpData, setRestSignUpData] = useState({
+    restaurant_name: "",
+    restaurant_email: "",
+    restaurant_address: "",
+    restaurant_openingtime: "",
+    restaurant_closingtime: "",
+    restaurant_password: "",
+  });
+  const [restaurantPasswordRule, setRestaurantPasswordRule] = useState(false);
+  const [restSignUpFieldsError, setRestSignUpFieldsError] = useState({});
+  const [restSignUpAuthError, setRestSignUpAuthError] = useState("");
+  const restSignUpValidDataObj = {
+    isRestNameValid: false,
+    isRestEmailValid: false,
+    isRestAddressValid: false,
+    isRestOpeningTimeValid: false,
+    isRestClosingTimeValid: false,
+    isRestPasswordValid: false,
+  };
+
+  const handleChangeRestSignUpData = (e) => {
+    setRestSignUpData((previousValue) => ({
+      ...previousValue,
+      [e.target.name]: e.target.value.trim(),
+    }));
+  };
+  const handleKeyUpRestPasswordRule = () => {
+    setRestaurantPasswordRule(true);
+  };
+  const validateRestName = () => {
+    if (restSignUpData.restaurant_name === "") {
+      setRestSignUpFieldsError((previousError) => ({
+        ...previousError,
+        restaurant_nameError: "Enter restaurant name",
+      }));
+      restSignUpValidDataObj["isRestNameValid"] = false;
+    } else {
+      setRestSignUpFieldsError((previousError) => ({
+        ...previousError,
+        restaurant_nameError: "",
+      }));
+      restSignUpValidDataObj["isRestNameValid"] = true;
+    }
+  };
+  const validateRestEmail = () => {
+    const emailRegx =
+      /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+    if (restSignUpData.restaurant_email === "") {
+      setRestSignUpFieldsError((previousError) => ({
+        ...previousError,
+        restaurant_emailError: "Enter restaurant email",
+      }));
+      restSignUpValidDataObj["isRestEmailValid"] = false;
+    } else if (emailRegx.test(restSignUpData.restaurant_email) === false) {
+      setRestSignUpFieldsError((previousError) => ({
+        ...previousError,
+        restaurant_emailError: "Invalid email address!, please check",
+      }));
+      restSignUpValidDataObj["isRestEmailValid"] = false;
+    } else {
+      setRestSignUpFieldsError((previousError) => ({
+        ...previousError,
+        restaurant_emailError: "",
+      }));
+      restSignUpValidDataObj["isRestEmailValid"] = true;
+    }
+  };
+  const validateRestAddress = () => {
+    if (restSignUpData.restaurant_address === "") {
+      setRestSignUpFieldsError((previousError) => ({
+        ...previousError,
+        restaurant_addressError: "Enter restaurant address",
+      }));
+      restSignUpValidDataObj["isRestAddressValid"] = false;
+    } else {
+      setRestSignUpFieldsError((previousError) => ({
+        ...previousError,
+        restaurant_addressError: "",
+      }));
+      restSignUpValidDataObj["isRestAddressValid"] = true;
+    }
+  };
+  const validateRestOpeningTime = () => {
+    if (restSignUpData.restaurant_openingtime === "") {
+      setRestSignUpFieldsError((previousError) => ({
+        ...previousError,
+        restaurant_openingTimeError: "Enter restaurant opening time",
+      }));
+      restSignUpValidDataObj["isRestOpeningTimeValid"] = false;
+    } else {
+      setRestSignUpFieldsError((previousError) => ({
+        ...previousError,
+        restaurant_openingTimeError: "",
+      }));
+      restSignUpValidDataObj["isRestOpeningTimeValid"] = true;
+    }
+  };
+  const validateRestClosingTime = () => {
+    if (restSignUpData.restaurant_closingtime === "") {
+      setRestSignUpFieldsError((previousError) => ({
+        ...previousError,
+        restaurant_closingTimeError: "Enter restaurant closing time",
+      }));
+      restSignUpValidDataObj["isRestClosingTimeValid"] = false;
+    } else {
+      setRestSignUpFieldsError((previousError) => ({
+        ...previousError,
+        restaurant_closingTimeError: "",
+      }));
+      restSignUpValidDataObj["isRestClosingTimeValid"] = true;
+    }
+  };
+  const validateRestPassword = () => {
+    if (restSignUpData.restaurant_password === "") {
+      setRestSignUpFieldsError((previousError) => ({
+        ...previousError,
+        restaurant_passwordError: "Enter restaurant password",
+      }));
+      restSignUpValidDataObj["isRestPasswordValid"] = false;
+    } else {
+      const passwordValidation =
+        /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*_=+-])/;
+      if (restSignUpData.restaurant_password.length < 8) {
+        setRestSignUpFieldsError((previousError) => ({
+          ...previousError,
+          restaurant_passwordError: "Password must have 8 characters",
+        }));
+        restSignUpValidDataObj["isRestPasswordValid"] = false;
+      } else if (
+        passwordValidation.test(restSignUpData.restaurant_password) === false
+      ) {
+        setRestSignUpFieldsError((previousError) => ({
+          ...previousError,
+          restaurant_passwordError: "Password is not valid",
+        }));
+        restSignUpValidDataObj["isRestPasswordValid"] = false;
+      } else if (restSignUpData.restaurant_password.length > 16) {
+        setRestSignUpFieldsError((previousError) => ({
+          ...previousError,
+          restaurant_passwordError: "Password can have maximum 16 characters",
+        }));
+        restSignUpValidDataObj["isRestPasswordValid"] = false;
+      } else {
+        setRestSignUpFieldsError((previousError) => ({
+          ...previousError,
+          restaurant_passwordError: "",
+        }));
+        restSignUpValidDataObj["isRestPasswordValid"] = true;
+      }
+    }
+    setRestaurantPasswordRule(false);
+  };
+  const handleClickRestSignUp = async () => {
+    setRestSignUpAuthError("");
+    validateRestName();
+    validateRestEmail();
+    validateRestAddress();
+    validateRestOpeningTime();
+    validateRestClosingTime();
+    validateRestPassword();
+    console.log(restSignUpValidDataObj);
+    if (Object.values(restSignUpValidDataObj).every(Boolean)) {
+      try {
+        const result = await axios.post(
+          "http://localhost:8080/restaurant/signup",
+          restSignUpData
+        );
+        if (result.status === 200) {
+          if (result.data.token === "restaurant-already-exists") {
+            setRestSignUpAuthError(result.data.msg);
+          } else {
+            console.log(result.data);
+            console.log("navigate to restaurant home page");
+          }
+        }
+      } catch (e) {
+        setRestSignUpAuthError(e.response.data.msg);
+      }
+    }
+  };
+  const gotoRestaurantLoginPage = () => {
     navigate("/restaurant/login");
-  }  
+  };
   return (
-    <div className='login-signup-main-container'>
-        <div className='login-signup-image-box'>
-            <img src={restaurant} alt="restaurantimage" className='login-signup-page-img'></img>
-        </div> 
-        <div className='login-signup-container'>
-            <div className='login-signup-form-box customer-login-signup-form-box'>
-                <h1>Restaurant Sign Up</h1>
-                <div className='login-signup-form-fields'>
-                    <label htmlFor="restaurant-name">Restaurant Name</label>
-                    <input type='text' id='restaurant-name' className='login-signup-form-input' name="restaurant-name" ></input>
-                </div>
-                <div className='login-signup-form-fields'>
-                    <label htmlFor="restaurant-email">Restaurant Email</label>
-                    <input type='email' id='restaurant-email' className='login-signup-form-input' name="restaurant-email" ></input>
-                </div>
-                <div className='login-signup-form-fields'>
-                    <label htmlFor="restaurant-address">Address of Restaurant</label>
-                    <input type='text' id='restaurant-address' className='login-signup-form-input' name="restaurant-address" ></input>
-                </div>
-                <div className='login-signup-form-fields'>
-                    <label htmlFor="restaurant-opening-time">Opening Time</label>
-                    <input type="time" id="restaurant-opening-time" className='login-signup-form-input' name="restaurant-opening-time"></input>
-                </div>
-                <div className='login-signup-form-fields'>
-                    <label htmlFor="restaurant-closing-time">Closing Time</label>
-                    <input type="time" id="restaurant-closing-time" className='login-signup-form-input' name="restaurant-closing-time"></input>
-                </div>
-                <div className='login-signup-form-fields'>
-                    <label htmlFor="restaurant-password">Password</label>
-                    <input type='password' id='restaurant-password' className='login-signup-form-input' name="restaurant-password" ></input>
-                </div>
-                <div className='login-signup-form-complete-btn-box'>
-                    <button className='login-signup-form-complete-btn customer-login-signup-btn'>Sign Up</button>
-                </div>
-                <div className='login-signup-link-box'>
-                    <p>Already have an account?<span className='login-sign-up-link' onClick={gotoRestaurantLoginPage}>Login</span></p>
-                </div>
-            </div>
+    <div className="login-signup-main-container">
+      <div className="login-signup-image-box">
+        <img
+          src={restaurant}
+          alt="restaurantimage"
+          className="login-signup-page-img"
+        ></img>
+      </div>
+      <div className="login-signup-container">
+        <div className="login-signup-form-box customer-login-signup-form-box">
+          <h1>Restaurant Sign Up</h1>
+          <div className="login-signup-form-fields">
+            <label htmlFor="restaurant-name">Restaurant Name</label>
+            <input
+              type="text"
+              id="restaurant-name"
+              className="login-signup-form-input"
+              name="restaurant_name"
+              onChange={handleChangeRestSignUpData}
+            ></input>
+            <p
+              className={
+                restSignUpFieldsError.restaurant_nameError === ""
+                  ? "hide-data"
+                  : "display-input-data-error"
+              }
+            >
+              {restSignUpFieldsError.restaurant_nameError}
+            </p>
+          </div>
+          <div className="login-signup-form-fields">
+            <label htmlFor="restaurant-email">Restaurant Email</label>
+            <input
+              type="email"
+              id="restaurant-email"
+              className="login-signup-form-input"
+              name="restaurant_email"
+              onChange={handleChangeRestSignUpData}
+            ></input>
+            <p
+              className={
+                restSignUpFieldsError.restaurant_emailError === ""
+                  ? "hide-data"
+                  : "display-input-data-error"
+              }
+            >
+              {restSignUpFieldsError.restaurant_emailError}
+            </p>
+          </div>
+          <div className="login-signup-form-fields">
+            <label htmlFor="restaurant-address">Address of Restaurant</label>
+            <textarea
+              id="restaurant-address"
+              className="login-signup-form-input address-input"
+              name="restaurant_address"
+              onChange={handleChangeRestSignUpData}
+            ></textarea>
+            <p
+              className={
+                restSignUpFieldsError.restaurant_addressError === ""
+                  ? "hide-data"
+                  : "display-input-data-error"
+              }
+            >
+              {restSignUpFieldsError.restaurant_addressError}
+            </p>
+          </div>
+          <div className="login-signup-form-fields">
+            <label htmlFor="restaurant-opening-time">Opening Time</label>
+            <input
+              type="time"
+              id="restaurant-opening-time"
+              className="login-signup-form-input"
+              name="restaurant_openingtime"
+              onChange={handleChangeRestSignUpData}
+            ></input>
+            <p
+              className={
+                restSignUpFieldsError.restaurant_openingTimeError === ""
+                  ? "hide-data"
+                  : "display-input-data-error"
+              }
+            >
+              {restSignUpFieldsError.restaurant_openingTimeError}
+            </p>
+          </div>
+          <div className="login-signup-form-fields">
+            <label htmlFor="restaurant-closing-time">Closing Time</label>
+            <input
+              type="time"
+              id="restaurant-closing-time"
+              className="login-signup-form-input"
+              name="restaurant_closingtime"
+              onChange={handleChangeRestSignUpData}
+            ></input>
+            <p
+              className={
+                restSignUpFieldsError.restaurant_closingTimeError === ""
+                  ? "hide-data"
+                  : "display-input-data-error"
+              }
+            >
+              {restSignUpFieldsError.restaurant_closingTimeError}
+            </p>
+          </div>
+          <div className="login-signup-form-fields">
+            <label htmlFor="restaurant-password">Password</label>
+            <input
+              type="password"
+              id="restaurant-password"
+              className="login-signup-form-input"
+              name="restaurant_password"
+              onChange={handleChangeRestSignUpData}
+              onKeyUp={handleKeyUpRestPasswordRule}
+            ></input>
+            <p
+              className={restaurantPasswordRule ? "password-rule" : "hide-data"}
+            >
+              Password must have 8 characters including 1 Uppercase, 1
+              lowercase, 1 number & 1 special character
+            </p>
+            <p
+              className={
+                restSignUpFieldsError.restaurant_passwordError === ""
+                  ? "hide-data"
+                  : "display-input-data-error"
+              }
+            >
+              {restSignUpFieldsError.restaurant_passwordError}
+            </p>
+            <p
+              className={
+                restSignUpAuthError === ""
+                  ? "hide-data"
+                  : "display-login-signup-auth-error"
+              }
+            >
+              {restSignUpAuthError}
+            </p>
+          </div>
+          <div className="login-signup-form-complete-btn-box">
+            <button
+              className="login-signup-form-complete-btn customer-login-signup-btn"
+              onClick={handleClickRestSignUp}
+            >
+              Sign Up
+            </button>
+          </div>
+          <div className="login-signup-link-box">
+            <p>
+              Already have an account?
+              <span
+                className="login-sign-up-link"
+                onClick={gotoRestaurantLoginPage}
+              >
+                Login
+              </span>
+            </p>
+          </div>
         </div>
+      </div>
     </div>
-  )
+  );
 }
